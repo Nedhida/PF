c581beb6042b446eb22c6680f5fca881303e3331
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-04 06:42:09 +0000

    [Change]footer to BEM

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+  end
+  
 end
