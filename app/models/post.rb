afdcb9d48b51cd6bd265c0effc0feae4b2e9cfdb
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-08 01:01:38 +0000

    [Make]favorites function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  #投稿が誰にいいねされているか取得
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
 
