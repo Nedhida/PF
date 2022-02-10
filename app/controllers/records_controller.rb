76c7eddf1b694712bfc3751f5f0d470adf21edfe
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-10 09:30:49 +0000

    [Fix]records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,7 +5,6 @@ class RecordsController < ApplicationController
 
   def income_day
     @start_time = params[:start_time]
-    @incomes = Income.order(created_at: :asc)
     @income_values = IncomeValue.where(start_time: @start_time)
     @income_value_total = 0
     @income_values.each do |income_value|
