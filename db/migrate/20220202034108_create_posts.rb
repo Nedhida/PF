6dad6fcb669808f0ecdc2dd889b4314dfc57fde8
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-11 15:28:37 +0000

    [Reset]migrate

--- db/migrate/20220202034108_create_posts.rb
+++ db/migrate/20220202034108_create_posts.rb
@@ -2,12 +2,13 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
 
-      t.references :user, foreign_key: true, null: false
+      t.integer :user_id, null: false
       t.string :title, null: false #タイトル
       t.string :image_id #投稿画像
       t.text :body, null: false #投稿内容
 
       t.timestamps
     end
+    add_foreign_key :posts, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_230642) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
