aa9ef3b19693bda220dee35bdf0ef308f305bdd0
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-21 01:26:05 +0000

    [Add]graphs show

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -32,10 +32,11 @@ class RecordsController < ApplicationController
 
   #月の収支グラフ
   def graph_month
-    # (1) * = 可変長引数、配列として認識される。(2) split("-") = 文字列を"-"で分断。(3)Time.local = Time オブジェクトを返す。 (4).all_month = その月の１日から月末まで。(5)where = テーブルから（４）の条件で取得。group_by_month = 月ごとにグループ化
-    # @start_time = "2022-02" =>  (1)["2022-02"] => (2)["2022", "02"] => (3)2022-02-01 00:00:00 +0000 => (4)2022-02-01 00:00:00 +0000..2022-02-28 23:59:59 +0000
+    # (1) * = 可変長引数、配列として認識される。    (2) split("-") = 文字列を"-"で分断。(3)Time.local = Time オブジェクトを返す。 (4).all_month = その月の１日から月末まで。(5)where = テーブルから（４）の条件で取得。group_by_month = 月ごとにグループ化
+    # @start_time = "2022-02" =>  (1)["2022-02"] => (2)["2022", "02"] =>                (3)2022-02-01 00:00:00 +0000 =>           (4)2022-02-01 00:00:00 +0000..2022-02-28 23:59:59 +0000
 
     #月の収入額合計
+    
     @in_value_month = IncomeValue.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
     @income_value_total = 0
     @in_value_month.each do |income_value|
@@ -58,7 +59,7 @@ class RecordsController < ApplicationController
   end
 
 
-  
+
   private
 
   def set_start_time
