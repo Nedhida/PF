ec8c24ef86b9d0ec3d115be71663ce656c34b751
Nedhida <pinedoutshadeyouth@gmail.com>
2022-02-22 13:37:47 +0000

    [Fix]layouts follw & set current user to records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,15 +1,15 @@
 class RecordsController < ApplicationController
   before_action :set_start_time
+  before_action :set_user
   before_action :income, only: %i[income_day day]
   before_action :fixedcost, only: %i[fixedcost_day day]
   before_action :variablecost, only: %i[variablecost_day day]
 
   #月の収支合計カレンダー
   def month
-    user = current_user
-    @income_values = user.income_values.order("start_time asc")
-    @fixedcost_values = user.fixedcost_values.order("start_time asc")
-    @variablecost_values = user.variablecost_values.order("start_time asc")
+    @income_values = @user.income_values.order("start_time asc")
+    @fixedcost_values = @user.fixedcost_values.order("start_time asc")
+    @variablecost_values = @user.variablecost_values.order("start_time asc")
     @all_values = @income_values + @fixedcost_values + @variablecost_values
   end
 
@@ -36,22 +36,21 @@ class RecordsController < ApplicationController
     # @start_time = "2022-02" =>  (1)["2022-02"] => (2)["2022", "02"] =>                (3)2022-02-01 00:00:00 +0000 =>           (4)2022-02-01 00:00:00 +0000..2022-02-28 23:59:59 +0000
 
     #月の収入額合計
-
-    @in_value_month = IncomeValue.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
+    @in_value_month = @user.income_values.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
     @income_value_total = 0
     @in_value_month.each do |income_value|
       @income_value_total += income_value.value
     end
 
     #月の固定費額合計
-    @fix_value_month = FixedcostValue.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
+    @fix_value_month = @user.fixedcost_values.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
     @fixedcost_value_total = 0
     @fix_value_month.each do |fixedcost_value|
       @fixedcost_value_total += fixedcost_value.value
     end
 
     #月の支出額合計
-    @var_value_month = VariablecostValue.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
+    @var_value_month = @user.variablecost_values.where(start_time: Time.local(*@start_time.split("-")).all_month)#.group_by_month(:start_time)
     @variablecost_value_total = 0
     @var_value_month.each do |variablecost_value|
       @variablecost_value_total +=  variablecost_value.value
@@ -69,9 +68,13 @@ class RecordsController < ApplicationController
     @start_time = params[:start_time]
   end
 
+  def set_user
+    @user = current_user
+  end
+
   def income
-    @incomes = Income.order(created_at: :asc)
-    @income_values = IncomeValue.where(start_time: @start_time)
+    @incomes = @user.incomes.order(created_at: :asc)
+    @income_values = @user.income_values.where(start_time: @start_time)
     @income_value_total = 0
     @income_values.each do |income_value|
       @income_value_total += income_value.value
@@ -79,8 +82,8 @@ class RecordsController < ApplicationController
   end
 
   def fixedcost
-    @fixedcosts = Fixedcost.order(created_at: :asc)
-    @fixedcost_values = FixedcostValue.where(start_time: @start_time)
+    @fixedcosts = @user.fixedcosts.order(created_at: :asc)
+    @fixedcost_values = @user.fixedcost_values.where(start_time: @start_time)
     @fixedcost_value_total = 0
     @fixedcost_values.each do |fixedcost_value|
       @fixedcost_value_total += fixedcost_value.value
@@ -88,8 +91,8 @@ class RecordsController < ApplicationController
   end
 
   def variablecost
-    @variablecosts = Variablecost.order(created_at: :asc)
-    @variablecost_values = VariablecostValue.where(start_time: @start_time)
+    @variablecosts = @user.variablecosts.order(created_at: :asc)
+    @variablecost_values = @user.variablecost_values.where(start_time: @start_time)
     @variablecost_value_total = 0
     @variablecost_values.each do |variablecost_value|
       @variablecost_value_total +=  variablecost_value.value
